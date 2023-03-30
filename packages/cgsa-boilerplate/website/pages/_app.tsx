eaedb32230ead29ca72a79c42dd27aaeb0f49811
mayank-96 <pagarmayank07@gmail.com>
2023-03-30 18:16:16 +0530

    fix: web boilerplate

--- packages/cgsa-boilerplate/website/pages/_app.tsx
+++ packages/cgsa-boilerplate/website/pages/_app.tsx
@@ -1,6 +1,9 @@
-
-import type { AppProps } from 'next/app'
-
+import type { AppProps } from 'next/app';
+import GluestackProvider from '@project/components';
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <GluestackProvider>
+      <Component {...pageProps} />
+    </GluestackProvider>
+  );
 }
--- packages/cgsa-boilerplate/website/pages/_document.tsx
+++ packages/cgsa-boilerplate/website/pages/_document.tsx
@@ -1,13 +1,26 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import * as React from 'react';
+import { Html, Head, Main, NextScript } from 'next/document';
+import { AppRegistry } from 'react-native-web';
+import { flush } from '@dank-style/react';
 
-export default function Document() {
+function Document() {
   return (
-    <Html lang="en">
+    <Html lang='en'>
       <Head />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
+
+Document.getInitialProps = async ({ renderPage }: any) => {
+  AppRegistry.registerComponent('Main', () => Main);
+  const { getStyleElement } = AppRegistry.getApplication('Main');
+  const page = await renderPage();
+  const styles = [getStyleElement(), ...flush()];
+  return { ...page, styles: React.Children.toArray(styles) };
+};
+
+export default Document;
