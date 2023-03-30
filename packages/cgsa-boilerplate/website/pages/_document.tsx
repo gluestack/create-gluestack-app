de20bf1a23f86f157f17d1075fa1f15efdd59120
mayank-96 <pagarmayank07@gmail.com>
2023-03-30 18:28:12 +0530

    fix: updated _document

--- packages/cgsa-boilerplate/website/pages/_document.tsx
+++ packages/cgsa-boilerplate/website/pages/_document.tsx
@@ -15,11 +15,28 @@ function Document() {
   );
 }
 
+export const style = `
+html, body, #__next {
+  width: 100%;
+  /* To smooth any scrolling behavior */
+  -webkit-overflow-scrolling: touch;
+  margin: 0px;
+  padding: 0px;
+  /* Allows content to fill the viewport and go beyond the bottom */
+  min-height: 100%;
+}`;
+
 Document.getInitialProps = async ({ renderPage }: any) => {
   AppRegistry.registerComponent('Main', () => Main);
   const { getStyleElement } = AppRegistry.getApplication('Main');
   const page = await renderPage();
-  const styles = [getStyleElement(), ...flush()];
+
+  const styles = [
+    // eslint-disable-next-line react/jsx-key
+    <style dangerouslySetInnerHTML={{ __html: style }} />,
+    getStyleElement(),
+    ...flush(),
+  ];
   return { ...page, styles: React.Children.toArray(styles) };
 };
 
