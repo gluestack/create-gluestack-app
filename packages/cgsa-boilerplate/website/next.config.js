8c12fc18fdb62b423a40debf073f6ae2d5c4c635
vidhi499 <katariavidhi99@gmail.com>
2023-03-30 13:38:29 +0530

    Revert "fix: text added"
    This reverts commit 044856ec302be458c20002289288830bb58b361a.

--- packages/cgsa-boilerplate/website/next.config.js
+++ packages/cgsa-boilerplate/website/next.config.js
@@ -1,11 +1,30 @@
 /** @type {import('next').NextConfig} */
 
-const { withGluestackUI } = require("@gluestack/ui-next-adapter");
+const withPlugins = require("next-compose-plugins");
+const withTM = require("next-transpile-modules")([
+  "react-native-web",
+  "@project/components"
+]);
 
 const nextConfig = {
   pageExtensions: ["tsx", "ts", "jsx", "js"],
   reactStrictMode: true,
-  transpilePackages: ["@project/components"],
+  // webpack5: true,
+  webpack: (config, { isServer }) => {
+    config.resolve.fallback = {
+      fs: false
+    };
+
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      // Transform all direct `react-native` imports to `react-native-web`
+      "react-native$": "react-native-web",
+    };
+
+    return config;
+  },
 };
 
-module.exports = withGluestackUI(nextConfig);
+module.exports = withPlugins([[withTM]], {
+  ...nextConfig
+});
